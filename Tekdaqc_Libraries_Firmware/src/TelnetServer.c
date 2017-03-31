2837e82d525ad99e42c3fc5d30e60d83d2049e6e
pchiacha <pdcha@ucdavis.edu>
2017-03-30 17:07:12 -0700

    update comment
    line 185

--- Tekdaqc_Libraries_Firmware/src/TelnetServer.c
+++ Tekdaqc_Libraries_Firmware/src/TelnetServer.c
@@ -182,7 +182,7 @@ static err_t TelnetAccept(void *arg, struct tcp_pcb *pcb, err_t err) {
 	tcp_nagle_enable(pcb);
 	telnet_server.pcb = pcb;
 	telnet_server.pcb->so_options |= SOF_KEEPALIVE;
-	telnet_server.pcb->keep_idle = 60000UL; // 2 seconds
+	telnet_server.pcb->keep_idle = 60000UL; // 1 minute
 	telnet_server.pcb->keep_intvl = 1000UL; // 1 Second
 	telnet_server.pcb->keep_cnt = 9; // 9 Consecutive failures terminate
 
